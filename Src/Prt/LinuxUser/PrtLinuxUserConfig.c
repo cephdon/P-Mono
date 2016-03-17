d53cdbcc71324e6668df9f1f86fda99ce8e017d1
jq.yang@berkeley.edu <jq.yang@berkeley.edu>
2016-03-17 14:03:44 -0700

    remove gitmodules and add linux stuff

---
+++ Src/Prt/LinuxUser/PrtLinuxUserConfig.c
@@ -0,0 +1,74 @@
+#include "PrtLinuxUserConfig.h"
+#include "Prt.h"
+
+PRT_RECURSIVE_MUTEX PRT_CALL_CONV PrtCreateMutex()
+{
+	pthread_mutexattr_t attr;
+	pthread_mutexattr_init(&attr);
+	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+	PRT_RECURSIVE_MUTEX mutex = malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(mutex, &attr);
+	PrtAssert(mutex != NULL, "Unable to create mutex");
+	return mutex;
+}
+
+void PRT_CALL_CONV PrtDestroyMutex(_In_ PRT_RECURSIVE_MUTEX mutex)
+{
+	int result = pthread_mutex_destroy(mutex);
+	free(mutex);
+	PrtAssert(result == 0, "Unable to release mutex");
+}
+
+void PRT_CALL_CONV PrtLockMutex(_In_ PRT_RECURSIVE_MUTEX mutex)
+{
+	int result = pthread_mutex_lock(mutex);
+	PrtAssert(result == 0, "Unable to wait for mutex");
+}
+
+void PRT_CALL_CONV PrtUnlockMutex(_In_ PRT_RECURSIVE_MUTEX mutex)
+{
+	int result = pthread_mutex_unlock(mutex);
+	PrtAssert(result == 0, "Unable to unlock mutex");
+}
+
+void * PRT_CALL_CONV PrtMalloc(_In_ size_t size)
+{
+	PrtAssert(size > 0, "Size must be positive to avoid platform-specific behavior");
+	void *ptr = malloc(size);
+	PrtAssert(ptr != NULL, "Memory allocation error");
+	return ptr;
+}
+
+void * PRT_CALL_CONV PrtCalloc(_In_ size_t nmemb, _In_ size_t size)
+{
+	PrtAssert(size > 0, "Size must be positive to avoid platform-specific behavior");
+	PrtAssert(nmemb > 0, "Size must be positive to avoid platform-specific behavior");
+
+	void *ptr = calloc(nmemb, size);
+	PrtAssert(ptr != NULL, "Memory allocation error");
+	return ptr;
+}
+
+void * PRT_CALL_CONV PrtRealloc(_Inout_ void *ptr, _In_ size_t size)
+{
+	PrtAssert(ptr != NULL, "Memory must be non-null to avoid platform-specific behavior");
+	PrtAssert(size > 0, "Size must be positive to avoid platform-specific behavior");
+
+	ptr = realloc(ptr, size);
+	PrtAssert(ptr != NULL, "Memory allocation error");
+	return ptr;
+}
+
+void PRT_CALL_CONV PrtFree(void *ptr)
+{
+	free(ptr);
+}
+
+PRT_BOOLEAN PRT_CALL_CONV PrtChoose()
+{
+	PRT_UINT32 value = rand();
+	if (value < RAND_MAX / 2)
+		return PRT_FALSE;
+	else
+		return PRT_TRUE;
+}
